d598a3b0a815e0e7aec10f672f86f903adf8fb05
DevanandGit <nandhudevanand4419@gmail.com>
2025-11-01 12:23:21 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ✅ Enable CORS before listen()
+  app.enableCors({
+    origin: '*',  // or your frontend origin like 'http://localhost:4200'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  console.log('JWT_SECRET:', process.env.JWT_SECRET);
-
 
 }
 bootstrap();
