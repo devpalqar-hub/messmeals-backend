09cfc7e800293be8053c504cc4d3f1a12d5590ae
DevanandGit <nandhudevanand4419@gmail.com>
2025-12-30 09:31:31 +0530

    new seeder added

--- src/twofactor/twofactor.service.ts
+++ src/twofactor/twofactor.service.ts
@@ -6,7 +6,8 @@ export class TwoFactorService {
     private readonly apiKey = process.env.TWO_FACTOR_API_KEY; // store key in .env
     private readonly template_name = process.env.TEMPLATE_NAME;
     async sendOtp(phone: string) {
-        const url = `https://2factor.in/API/V1/${this.apiKey}/SMS/${phone}/AUTOGEN3/:${this.template_name}`;
+        const url = `https://2factor.in/API/V1/${this.apiKey}/SMS/${phone}/AUTOGEN3/MESSMEALS+OTP`;
+        console.log(url)
         const { data } = await axios.get(url);
         return data;  // returns details like OTP session ID
     }
