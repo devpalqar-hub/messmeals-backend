64bca0b35e5b51b0d57b175b763729d88eb28a96
DevanandGit <nandhudevanand4419@gmail.com>
2025-12-30 11:00:39 +0530

    otp made 6

--- src/twofactor/twofactor.service.ts
+++ src/twofactor/twofactor.service.ts
@@ -6,7 +6,7 @@ export class TwoFactorService {
     private readonly apiKey = process.env.TWO_FACTOR_API_KEY; // store key in .env
     private readonly template_name = process.env.TEMPLATE_NAME;
     async sendOtp(phone: string) {
-        const url = `https://2factor.in/API/V1/${this.apiKey}/SMS/${phone}/AUTOGEN3/MESSMEALS+OTP`;
+        const url = `https://2factor.in/API/V1/${this.apiKey}/SMS/${phone}/AUTOGEN/MESSMEALS+OTP`;
         console.log(url)
         const { data } = await axios.get(url);
         return data;  // returns details like OTP session ID
